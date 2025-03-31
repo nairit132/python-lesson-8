4bf64f18e1f5f89662ec32f8395c41d984747a85
nairit132 <nairithmajmundar@gmail.com>
2025-03-31 19:40:33 +0100

    Add files via upload

---
+++ task three.py
@@ -0,0 +1,10 @@
+mean1 = int(input("What was the mean"))
+wrong_number = int(input("What was the wrong number?"))
+right_number = int(input("What was the right number?"))
+total_mean = int(input("What was the wrong mean?"))
+sum = mean1 * total_mean
+print("the sum of fourty number :", sum)
+num_two = sum - ((wrong_number) - (right_number))
+print("((wrong_number) - (right_number))  :", num_two )
+mean_two = num_two/total_mean 
+print(mean_two)
