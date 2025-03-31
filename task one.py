9796d803c6fc2c95a5130bf2a90988f984f37db4
nairit132 <nairithmajmundar@gmail.com>
2025-03-31 19:09:16 +0100

    Add files via upload

---
+++ task one.py
@@ -0,0 +1,15 @@
+a = int(input("Place number 1"))
+b = int(input("Place number 2"))
+c = int(input("Place number 3"))
+d = int(input("Place number 4"))
+e = 0
+e = (a+b) * c / d
+print("value of (a+b) * c / d is", e)
+
+name = str(input("What is your name?"))
+age  = int(input("What is your age?"))
+
+if name == 'Nairit' and age == 13 :
+    print("Welcome back Nairt!")
+else:
+    print("Leave or try again")
